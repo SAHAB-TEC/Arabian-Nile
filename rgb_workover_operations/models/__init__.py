dea20dd1418026a238071f56733d695351ac7887
ragabmahmoud1 <ragabdeaf93@outlook.com>
2026-07-29 10:33:29 +0300

    Increment version to 18.0.1.0.11. Add Workover Rig Summary model and associated views for generating summary reports. Implement report export functionality to Excel and PDF. Update menu actions and access rights for the new report feature, and refactor existing report actions for consistency.

--- rgb_workover_operations/models/__init__.py
+++ rgb_workover_operations/models/__init__.py
@@ -5,3 +5,4 @@ from . import workover_operator
 from . import workover_time_category
 from . import workover_daily_report
 from . import workover_present_operation
+from . import workover_rig_summary
--- rgb_workover_operations/wizard/__init__.py
+++ rgb_workover_operations/wizard/__init__.py
@@ -2,4 +2,3 @@
 from . import excel_report_helper
 from . import daily_report_export_wizard
 from . import summary_report_export_wizard
-from . import rig_summary_wizard
